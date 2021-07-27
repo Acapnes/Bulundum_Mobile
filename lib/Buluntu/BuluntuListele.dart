1220681ba7edd9362a8526a5f9cf17520993fdf7
asalp <as.alper1@hotmail.com>
2021-07-27 14:59:23 +0300

    founditem list edited

--- lib/Buluntu/BuluntuListele.dart
+++ lib/Buluntu/BuluntuListele.dart
@@ -10,7 +10,6 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MainBuluntuList extends StatefulWidget {
-
   @override
   _MainBuluntuListState createState() => _MainBuluntuListState();
 }
@@ -20,17 +19,14 @@ class _MainBuluntuListState extends State<MainBuluntuList> {
   List data;
 
   Future<String> ListFoundItems() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    var endpointUrl = 'https://www.myurl.com/api/v1/user';
-    Map<String, String> queryParams = {
-      'param1': 'sharedPreferences.get("sk1");',
-      'param2': 'sharedPreferences.get("sk2");'
-    };
-    var response = await http.get(Uri.parse("https://dev.bulundum.com/api/v3/founditems"),
-        headers: {
-          'Accept':'application/json',
-        });
-    var jsonData = jsonDecode(response.body);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final uri = Uri.https("dev.bulundum.com", "/api/v3/founditems",
+        {'sk1': prefs.get("sk1"), 'sk2': prefs.get("sk2")});
+    var response = await http.get(uri);
+    setState(() {
+      var jsonData = jsonDecode(response.body);
+      data = jsonData["Records"];
+    });
   }
 
   @override
@@ -40,6 +36,8 @@ class _MainBuluntuListState extends State<MainBuluntuList> {
     this.ListFoundItems();
   }
 
+  //Eşyano - kayıtno - eşyaismi - özeldetaylar - foto
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -48,25 +46,62 @@ class _MainBuluntuListState extends State<MainBuluntuList> {
         drawer: mainDrawer(),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.search),
-          onPressed: () {
-            ListFoundItems();
-          },
+          onPressed: () {},
         ),
         body: ListView.builder(
           itemCount: data == null ? 0 : data.length,
-          itemBuilder: (context,int index){
-            return Container(
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Card(
-                      child: Container(
-                          child : Text(data[0]["Slug"]),
-                        padding: EdgeInsets.all(20),
+          itemBuilder: (context, int index) {
+            return GestureDetector(
+              onTap: (){
+                number = index;
+                print(number);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => mainBuluntuDetaylar(number: number)));
+              },
+              child: Container(
+                margin: EdgeInsets.all(15),
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Card(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 160,
+                          child: Column(
+                            children: [
+                              Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(top: 20.0),
+                                  child: Text(data[index]["Title"],style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                                ),
+                              ),
+                              Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: ListTile(
+                                      title:
+                                          Text("Eşya : " + data[index]["Title"],style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold)),
+                                      subtitle:
+                                          Text("Eşya no : " + data[index]["Id"],style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold)),
+                                      trailing: data[index]["Logo"] == null
+                                          ? Image(
+                                              image: NetworkImage(
+                                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),height: 50,width: 50,)
+                                          : Image(
+                                              image: NetworkImage(
+                                                  data[index]["Logo"])))),
+                              Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(bottom: 10.0),
+                                  child: Text("Detaylar",style: TextStyle(fontSize: 18),),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    )
-                  ],
+                    ],
+                  ),
                 ),
               ),
             );
@@ -75,10 +110,4 @@ class _MainBuluntuListState extends State<MainBuluntuList> {
       ),
     );
   }
-}
-
-class User {
-  final String Id, Title, Slug;
-
-  User(this.Id, this.Title, this.Slug);
 }
