1d97988540ac5ef73b2bfa473280f36114d5e223
asalp <as.alper1@hotmail.com>
2021-08-29 16:46:37 +0300

    Added introduction_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:bulundum_mobile/Buluntu/BuluntuListele.dart';
-import 'package:bulundum_mobile/Drawer/mainDrawer.dart';
 import 'package:bulundum_mobile/Login-Register/LoginPage.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -109,12 +110,12 @@ class _MyHomePageState extends State<MyHomePage>
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => MainBuluntuList()));
+                              builder: (context) => OnBoardingPage()));  //***MainBuluntuList
                     } else {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => LoginMain()));
+                              builder: (context) => LoginMain()));  //***LoginMain
                     }
                   },
                   child: Container(
@@ -130,110 +131,167 @@ class _MyHomePageState extends State<MyHomePage>
     );
   }
 }
+final introKey = GlobalKey<IntroductionScreenState>();
 
-class SecondPage extends StatefulWidget {
-  @override
-  _SecondPageState createState() => _SecondPageState();
+void _onIntroEnd(context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(builder: (_) => MainBuluntuList()),
+  );
 }
 
-class _SecondPageState extends State<SecondPage> {
-  String sk1 = "", sk2 = "", loggedIn = "", go = "MyHomePage";
+Widget _buildFullscrenImage() {
+  return Image.asset(
+    'assets/fullscreen.jpg',
+    fit: BoxFit.cover,
+    height: double.infinity,
+    width: double.infinity,
+    alignment: Alignment.center,
+  );
+}
 
-  @override
-  void initState() {
-    getData();
-
-    if (sk1 != null && sk2 != null) {
-      loggedIn = "Oturum Kapat";
-    } else {
-      loggedIn = "Oturum Aç";
-    }
-  }
-
-  getData() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    sk1 = sharedPreferences.get("Username");
-    sk2 = sharedPreferences.get("Password");
-    print(sk1);
-    print(sk2);
-  }
-
-  ElevatedButton CustomButton() {
-    return ElevatedButton(
-      child:
-      Text(loggedIn, style: TextStyle(color: Colors.white, fontSize: 18)),
-      onPressed: () {
-        if (loggedIn == "Oturum Aç") {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => LoginMain()));
-        } else {
-          print("Oturumu Kapatma kodu");
-        }
-      },
-    );
-  }
-
-  String loginButton = "Oturum Aç";
-
+Widget _buildImage(String assetName, [double width = 350]) {
+  return Image.asset('assets/$assetName', width: width);
+}
+class OnBoardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("2. Sayfa"),
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Colors.white,
+      globalHeader: Align(
+        alignment: Alignment.topRight,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16, right: 16),
+            child: _buildImage('flutter.png', 100),
+          ),
+        ),
       ),
-      drawer: mainDrawer(),
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 2.5,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          "Kaybettiğinize Ulaşın",
-                          style:
-                          TextStyle(fontSize: 35, color: Colors.blueAccent),
-                        )),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          "Bulduğunuzu Ulaştırın",
-                          style: TextStyle(
-                              fontSize: 35, color: Colors.orangeAccent),
-                        )),
-                  ),
-                ],
+      globalFooter: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: ElevatedButton(
+          child: const Text(
+            'Let\s go right away!',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () => _onIntroEnd(context),
+        ),
+      ),
+      pages: [
+        PageViewModel(
+          title: "Fractional shares",
+          body:
+          "Instead of having to buy an entire share, invest any amount you want.",
+          image: _buildImage('img1.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Learn as you go",
+          body:
+          "Download the Stockpile app and master the market with our mini-lesson.",
+          image: _buildImage('img2.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Kids and teens",
+          body:
+          "Kids and teens can track their stocks 24/7 and place trades that you approve.",
+          image: _buildImage('img3.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Full Screen Page",
+          body:
+          "Pages can be full screen as well.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id euismod lectus, non tempor felis. Nam rutrum rhoncus est ac venenatis.",
+          image: _buildFullscrenImage(),
+          decoration: pageDecoration.copyWith(
+            contentMargin: const EdgeInsets.symmetric(horizontal: 16),
+            fullScreen: true,
+            bodyFlex: 2,
+            imageFlex: 3,
+          ),
+        ),
+        PageViewModel(
+          title: "Another title page",
+          body: "Another beautiful body text for this example onboarding",
+          image: _buildImage('img2.jpg'),
+          footer: ElevatedButton(
+            onPressed: () {
+              introKey.currentState?.animateScroll(0);
+            },
+            child: const Text(
+              'FooButton',
+              style: TextStyle(color: Colors.white),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.lightBlue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
               ),
             ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Container(
-                    child: ElevatedButton(
-                      child: Text("Tanıtım",
-                          style: TextStyle(color: Colors.white, fontSize: 18)),
-                      onPressed: () {},
-                    ),
-                  ),
-                  CustomButton(),
-                ],
-              ),
-            )
-          ],
+          ),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Title of last page - reversed",
+          bodyWidget: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Click on ", style: bodyStyle),
+              Icon(Icons.edit),
+              Text(" to edit a post", style: bodyStyle),
+            ],
+          ),
+          decoration: pageDecoration.copyWith(
+            bodyFlex: 2,
+            imageFlex: 4,
+            bodyAlignment: Alignment.bottomCenter,
+            imageAlignment: Alignment.topCenter,
+          ),
+          image: _buildImage('img1.jpg'),
+          reverse: true,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipFlex: 0,
+      nextFlex: 0,
+      //rtl: true, // Display as right-to-left
+      skip: const Text('Skip'),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        color: Colors.black87,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
         ),
       ),
     );
   }
 }
+
