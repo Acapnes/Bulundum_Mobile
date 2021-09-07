8adf0e96ee11df399d2f6c799d32f87528c5b01c
asalp <as.alper1@hotmail.com>
2021-09-07 15:47:44 +0300

    Added intro slider

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:introduction_screen/introduction_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,9 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(fontFamily: 'Ubuntu'),
       home: MyHomePage(),
     );
   }
@@ -28,11 +25,14 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  PageController pageController = PageController(initialPage: 0);
+
   AnimationController _controller;
   String sk1 = "", sk2 = "";
   bool loggedIn = false, animRunn = false;
+
+  int currenrPage=0;
 
   @override
   void initState() {
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage>
         duration: const Duration(seconds: 1),
         vsync: this,
       )..forward();
-      animRunn = true;
+      animRunn = false;
     }
     getData();
   }
@@ -53,245 +53,217 @@ class _MyHomePageState extends State<MyHomePage>
     print(sk1);
     print(sk2);
   }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     Animation<RelativeRect> _animation = RelativeRectTween(
       begin: RelativeRect.fromLTRB(0, 0, 0, MediaQuery.of(context).size.height),
       end: RelativeRect.fromLTRB(
-          0, 0, 0, MediaQuery.of(context).size.height / 2),
+          0, 0, 0, MediaQuery.of(context).size.height / 3),
     ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
 
     Animation<RelativeRect> _animation2 = RelativeRectTween(
       begin:
-      RelativeRect.fromLTRB(0, 0, 0, -MediaQuery.of(context).size.height),
+          RelativeRect.fromLTRB(0, 0, 0, -MediaQuery.of(context).size.height),
       end: RelativeRect.fromLTRB(
           0, 0, 0, -MediaQuery.of(context).size.height / 4),
     ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
 
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(15),
-        child: Stack(
-          children: <Widget>[
-            PositionedTransition(
-              rect: _animation,
-              child: FadeTransition(
-                opacity: _controller,
-                child: Container(
-                  margin: EdgeInsets.only(top: 80),
-                  child: Image(
-                    image: AssetImage("img/icon.png"),
-                  ),
+      appBar: AppBar(title: Text("Bulundum"),centerTitle: true,
+        actions: [
+          IconButton(icon: Icon(Icons.arrow_back_ios),onPressed: (){
+            setState(() {
+              if(currenrPage!=0){
+                currenrPage--;
+              }
+            });
+            pageController.animateToPage(currenrPage, duration: Duration(milliseconds: 500), curve: Curves.easeOutSine);
+          },),
+
+          IconButton(icon: Icon(Icons.arrow_forward_ios),onPressed: (){
+            if(currenrPage!=2){
+              currenrPage++;
+            }
+            pageController.animateToPage(currenrPage, duration: Duration(milliseconds: 500), curve: Curves.easeOutSine);
+          },),
+        ],
+      ),
+        body: PageView(
+      pageSnapping: true,
+      controller: pageController,
+      onPageChanged: (index) {
+        print(index);
+        if (index == 2) {
+          setState(() {
+            if (animRunn == false) {
+              _controller = AnimationController(
+                duration: const Duration(seconds: 1),
+                vsync: this,
+              )..forward();
+              animRunn = false;
+            }
+          });
+        }
+      },
+      children: [
+        // --- Page 1 ---
+        Container(
+          margin: EdgeInsets.all(15),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                margin: EdgeInsets.only(top: 80),
+                child: Image(
+                  image: AssetImage("img/icon.png"),
                 ),
               ),
-            ),
-            PositionedTransition(
-              rect: _animation2,
-              child: FadeTransition(
-                opacity: _controller,
-                child: Container(
-                  margin: EdgeInsets.only(top: 80),
-                  child: Image(
-                    image: AssetImage("img/bulundum-yazi.png"),
-                  ),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                width: 70,
-                height: 90,
-                child: GestureDetector(
-                  onTap: () {
-                    initState();
+              Container(
+                  margin: EdgeInsets.only(top: 50, left: 25, right: 25),
+                  child: Text(
+                    "Artık eşyanıza kavuşmanın ve eşyaları sahiplerine kavuşturmanın etkili bir yolu var.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 25, fontFamily: "Ubuntu"),
+                  )),
+              Container(
+                height: 50,
+                width: 80,
+                margin:EdgeInsets.only(top: 80),
+                child: ElevatedButton(
+                  onPressed: (){
                     if (sk1 != null && sk2 != null) {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => OnBoardingPage()));  //***MainBuluntuList
+                              builder: (context) =>
+                                  MainBuluntuList())); //***MainBuluntuList
                     } else {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => LoginMain()));  //***LoginMain
+                              builder: (context) =>
+                                  LoginMain())); //***LoginMain
                     }
                   },
-                  child: Container(
-                      child: Image(
-                        image: AssetImage("img/right_arrow.png"),
-                      )),
+                  child: Text("Atla",style: TextStyle(fontSize: 18,fontFamily: "Ubuntu"),),
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-final introKey = GlobalKey<IntroductionScreenState>();
-
-void _onIntroEnd(context) {
-  Navigator.of(context).push(
-    MaterialPageRoute(builder: (_) => MainBuluntuList()),
-  );
-}
-
-Widget _buildFullscrenImage() {
-  return Image.asset(
-    'assets/fullscreen.jpg',
-    fit: BoxFit.cover,
-    height: double.infinity,
-    width: double.infinity,
-    alignment: Alignment.center,
-  );
-}
-
-Widget _buildImage(String assetName, [double width = 350]) {
-  return Image.asset('assets/$assetName', width: width);
-}
-class OnBoardingPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    const bodyStyle = TextStyle(fontSize: 19.0);
-
-    const pageDecoration = const PageDecoration(
-      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
-      bodyTextStyle: bodyStyle,
-      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-      pageColor: Colors.white,
-      imagePadding: EdgeInsets.zero,
-    );
-
-    return IntroductionScreen(
-      key: introKey,
-      globalBackgroundColor: Colors.white,
-      globalHeader: Align(
-        alignment: Alignment.topRight,
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 16, right: 16),
-            child: _buildImage('flutter.png', 100),
-          ),
-        ),
-      ),
-      globalFooter: SizedBox(
-        width: double.infinity,
-        height: 60,
-        child: ElevatedButton(
-          child: const Text(
-            'Let\s go right away!',
-            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-          ),
-          onPressed: () => _onIntroEnd(context),
-        ),
-      ),
-      pages: [
-        PageViewModel(
-          title: "Fractional shares",
-          body:
-          "Instead of having to buy an entire share, invest any amount you want.",
-          image: _buildImage('img1.jpg'),
-          decoration: pageDecoration,
-        ),
-        PageViewModel(
-          title: "Learn as you go",
-          body:
-          "Download the Stockpile app and master the market with our mini-lesson.",
-          image: _buildImage('img2.jpg'),
-          decoration: pageDecoration,
-        ),
-        PageViewModel(
-          title: "Kids and teens",
-          body:
-          "Kids and teens can track their stocks 24/7 and place trades that you approve.",
-          image: _buildImage('img3.jpg'),
-          decoration: pageDecoration,
-        ),
-        PageViewModel(
-          title: "Full Screen Page",
-          body:
-          "Pages can be full screen as well.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id euismod lectus, non tempor felis. Nam rutrum rhoncus est ac venenatis.",
-          image: _buildFullscrenImage(),
-          decoration: pageDecoration.copyWith(
-            contentMargin: const EdgeInsets.symmetric(horizontal: 16),
-            fullScreen: true,
-            bodyFlex: 2,
-            imageFlex: 3,
-          ),
-        ),
-        PageViewModel(
-          title: "Another title page",
-          body: "Another beautiful body text for this example onboarding",
-          image: _buildImage('img2.jpg'),
-          footer: ElevatedButton(
-            onPressed: () {
-              introKey.currentState?.animateScroll(0);
-            },
-            child: const Text(
-              'FooButton',
-              style: TextStyle(color: Colors.white),
-            ),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.lightBlue,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-            ),
-          ),
-          decoration: pageDecoration,
-        ),
-        PageViewModel(
-          title: "Title of last page - reversed",
-          bodyWidget: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text("Click on ", style: bodyStyle),
-              Icon(Icons.edit),
-              Text(" to edit a post", style: bodyStyle),
             ],
           ),
-          decoration: pageDecoration.copyWith(
-            bodyFlex: 2,
-            imageFlex: 4,
-            bodyAlignment: Alignment.bottomCenter,
-            imageAlignment: Alignment.topCenter,
+        ),
+        // --- Page 2 ---
+        Container(
+          margin: EdgeInsets.all(15),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                margin: EdgeInsets.only(top: 80),
+                child: Image(
+                  image: AssetImage("img/icon.png"),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: 50, left: 25, right: 25),
+                  child: Text(
+                    "Kolay, hızlı ve sıkıntısız.\n\nBuluntu kaydı\nKayıp eşya bildirme",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 25, fontFamily: "Ubuntu"),
+                  )),
+              Container(
+                height: 50,
+                width: 80,
+                margin:EdgeInsets.only(top: 80),
+                child: ElevatedButton(
+                  onPressed: (){
+                    if (sk1 != null && sk2 != null) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  MainBuluntuList())); //***MainBuluntuList
+                    } else {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  LoginMain())); //***LoginMain
+                    }
+                  },
+                  child: Text("Atla",style: TextStyle(fontSize: 18,fontFamily: "Ubuntu"),),
+                ),
+              ),
+            ],
           ),
-          image: _buildImage('img1.jpg'),
-          reverse: true,
+        ),
+        // --- Page 3 ---
+        Container(
+          margin: EdgeInsets.all(15),
+          child: Stack(
+            children: <Widget>[
+              PositionedTransition(
+                rect: _animation,
+                child: FadeTransition(
+                  opacity: _controller,
+                  child: Container(
+                    margin: EdgeInsets.only(top: 50),
+                    child: Image(
+                      image: AssetImage("img/icon.png"),
+                    ),
+                  ),
+                ),
+              ),
+              PositionedTransition(
+                rect: _animation2,
+                child: FadeTransition(
+                  opacity: _controller,
+                  child: Container(
+                   margin: EdgeInsets.only(top: 80),
+                    child: Image(
+                      image: AssetImage("img/bulundum-yazi.png"),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  width: 70,
+                  height: 90,
+                  child: GestureDetector(
+                    onTap: () {
+                      initState();
+                      if (sk1 != null && sk2 != null) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    MainBuluntuList())); //***MainBuluntuList
+                      } else {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    LoginMain())); //***LoginMain
+                      }
+                    },
+                    child: Container(
+                        child: Image(
+                      image: AssetImage("img/right_arrow.png"),
+                    )),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
-      onDone: () => _onIntroEnd(context),
-      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
-      showSkipButton: true,
-      skipFlex: 0,
-      nextFlex: 0,
-      //rtl: true, // Display as right-to-left
-      skip: const Text('Skip'),
-      next: const Icon(Icons.arrow_forward),
-      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
-      curve: Curves.fastLinearToSlowEaseIn,
-      controlsMargin: const EdgeInsets.all(16),
-      controlsPadding: kIsWeb
-          ? const EdgeInsets.all(12.0)
-          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-      dotsDecorator: const DotsDecorator(
-        size: Size(10.0, 10.0),
-        color: Color(0xFFBDBDBD),
-        activeSize: Size(22.0, 10.0),
-        activeShape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(25.0)),
-        ),
-      ),
-      dotsContainerDecorator: const ShapeDecoration(
-        color: Colors.black87,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-        ),
-      ),
-    );
+    ));
   }
 }
-
