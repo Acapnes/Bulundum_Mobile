84b6f732dbbc3255ac728882f9513f8029049d70
Alper <61701011+Acapnes@users.noreply.github.com>
2021-10-02 15:56:15 +0300

    globals library added

---
+++ lib/globals.dart
@@ -0,0 +1,15 @@
+library myApp.globals;
+
+bool isLoggedIn = false;
+String sk1 = "";
+String sk2 = "";
+String server = "production";
+String CompanyType = "";
+
+getUrl (String getUrl) {
+  if(server == "production"){
+    return "https://bulundum.com/api/v3/$getUrl";
+  }else{
+    return "https://dev.bulundum.com/api/v3/$getUrl";
+  }
+}
