bef29014c3ecd13d870b49fff41723a29379a095
Alper <61701011+Acapnes@users.noreply.github.com>
2021-11-06 03:19:01 +0300

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,43 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../globals.dart' as globals;
 
+class Init {
+  getSK() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    globals.sk1 = sharedPreferences.get("sk1") ?? "" ;
+    globals.sk2 = sharedPreferences.get("sk2") ?? "" ;
+    print(globals.sk1);
+  }
+
+  Init._();
+  static final instance = Init._();
+  Future initialize() async {
+    /// Uygulama açılırken çekilecek kodlar buraya yazılacak.
+    getSK();
+    await Future.delayed(Duration(milliseconds: 2000));
+  }
+}
 
 void main() {
-  runApp(MaterialApp(
-    title: "örnek",
-    theme: ThemeData.light(),
-    initialRoute: '/',
-    routes: {
-      '/': (context) => mainLoadingPage(),
-    }));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Ubuntu'),
-      home: MyHomePage(),
+    return FutureBuilder(
+      future: Init.instance.initialize(),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(home: mainLoadingPage());
+        } else {
+          if(globals.sk1 !="" && globals.sk2 != ""){
+            return MaterialApp(home: MainBuluntuList());
+          } else{
+            return MaterialApp(home: MyHomePage());
+          }
+        }
+      },
     );
   }
 }
@@ -35,35 +54,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   PageController pageController = PageController(initialPage: 0);
-  AnimationController _controller;
   bool animRunn = false;
   int currenrPage=0;
 
-  @override
-  void initState() {
-    if (animRunn == false) {
-      _controller = AnimationController(
-        duration: const Duration(seconds: 1),
-        vsync: this,
-      )..forward();
-      animRunn = false;
-    }
-    getData();
-  }
-
-    getData() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    globals.sk1 = sharedPreferences.get("sk1") ?? "" ;
-    globals.sk2 = sharedPreferences.get("sk2") ?? "" ;
-    if(globals.sk1 != "" && globals.sk2 != ""){
-      await Future.delayed(Duration(seconds: 1));
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) =>
-                  MainBuluntuList()));
-    }
-  }
   @override
   void dispose() {
     // TODO: implement dispose
@@ -72,192 +65,104 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    Animation<RelativeRect> _animation = RelativeRectTween(
-      begin: RelativeRect.fromLTRB(0, 0, 0, MediaQuery.of(context).size.height),
-      end: RelativeRect.fromLTRB(
-          0, 0, 0, MediaQuery.of(context).size.height / 3),
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
-
-    Animation<RelativeRect> _animation2 = RelativeRectTween(
-      begin:
-          RelativeRect.fromLTRB(0, 0, 0, -MediaQuery.of(context).size.height),
-      end: RelativeRect.fromLTRB(
-          0, 0, 0, -MediaQuery.of(context).size.height / 4),
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
-
     return Scaffold(
         body: PageView(
-      pageSnapping: true,
-      controller: pageController,
-      onPageChanged: (index) {
-        print(index);
-        if (index == 2) {
-          setState(() {
-            if (animRunn == false) {
-              _controller = AnimationController(
-                duration: const Duration(seconds: 1),
-                vsync: this,
-              )..forward();
-              animRunn = false;
-            }
-          });
-        }
-      },
-      children: [
-        // --- Page 1 ---
-        Container(
-          margin: EdgeInsets.only(top: 60),
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                margin: EdgeInsets.only(top: 80),
-                child: Image(
-                  image: AssetImage("assets/icon-v1.png"),
-                ),
-              ),
-              Container(
-                  margin: EdgeInsets.only(top: 40, left: 25, right: 25),
-                  child: Text(
-                    "Artık eşyanıza kavuşmanın ve eşyaları sahiplerine kavuşturmanın etkili bir yolu var.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 25, fontFamily: "Ubuntu"),
-                  )),
-              Container(
-                height: 50,
-                width: 80,
-                margin:EdgeInsets.only(top: 80),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    SharedPreferences prefs = await SharedPreferences.getInstance();
-                    if (prefs.get("sk1") != null && prefs.get("sk2")  != null) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  MainBuluntuList())); //***MainBuluntuList
-                    } else {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  LoginMain())); //***LoginMain
-                    }
-                  },
-                  child: Text("Atla",style: TextStyle(fontSize: 18,fontFamily: "Ubuntu"),),
-                ),
-              ),
-            ],
-          ),
-        ),
-        // --- Page 2 ---
-        Container(
-          margin: EdgeInsets.only(top: 60),
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                margin: EdgeInsets.only(top: 80),
-                child: Image(
-                  image: AssetImage("assets/icon-v1.png"),
-                ),
-              ),
-              Container(
-                  margin: EdgeInsets.only(top: 40, left: 25, right: 25),
-                  child: Text(
-                    "Kolay, hızlı ve sıkıntısız.\n\nBuluntu kaydı\nKayıp eşya bildirme",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 25, fontFamily: "Ubuntu"),
-                  )),
-              Container(
-                height: 50,
-                width: 80,
-                margin:EdgeInsets.only(top: 80),
-                child: ElevatedButton(
-                  onPressed: ()async {
-                    SharedPreferences prefs = await SharedPreferences.getInstance();
-                    if (prefs.get("sk1") != null && prefs.get("sk2")  != null) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  MainBuluntuList())); //***MainBuluntuList
-                    } else {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  LoginMain())); //***LoginMain
-                    }
-                  },
-                  child: Text("Atla",style: TextStyle(fontSize: 18,fontFamily: "Ubuntu"),),
-                ),
-              ),
-            ],
-          ),
-        ),
-        // --- Page 3 ---
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 30,vertical: 50),
-          child: Stack(
-            children: <Widget>[
-              PositionedTransition(
-                rect: _animation,
-                child: FadeTransition(
-                  opacity: _controller,
-                  child: Container(
-                    margin: EdgeInsets.only(top: 50),
+          pageSnapping: true,
+          controller: pageController,
+          children: [
+            // --- Page 1 ---
+            Container(
+              margin: EdgeInsets.only(top: 60),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: MediaQuery.of(context).size.height / 3,
+                    margin: EdgeInsets.only(top: 80),
                     child: Image(
                       image: AssetImage("assets/icon-v1.png"),
                     ),
                   ),
-                ),
-              ),
-              PositionedTransition(
-                rect: _animation2,
-                child: FadeTransition(
-                  opacity: _controller,
-                  child: Container(
-                   margin: EdgeInsets.only(top: 80),
-                    child: Image(
-                      image: AssetImage("img/bulundum-yazi.png"),
+                  Container(
+                      margin: EdgeInsets.only(top: 40, left: 25, right: 25),
+                      child: Text(
+                        "Artık eşyanıza kavuşmanın ve eşyaları sahiplerine kavuşturmanın etkili bir yolu var.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 25, fontFamily: "Ubuntu"),
+                      )),
+                  Container(
+                    height: 50,
+                    width: 80,
+                    margin:EdgeInsets.only(top: 80),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        SharedPreferences prefs = await SharedPreferences.getInstance();
+                        if (prefs.get("sk1") != null && prefs.get("sk2")  != null) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      MainBuluntuList())); //***MainBuluntuList
+                        } else {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      LoginMain())); //***LoginMain
+                        }
+                      },
+                      child: Text("Atla",style: TextStyle(fontSize: 18,fontFamily: "Ubuntu"),),
                     ),
                   ),
-                ),
+                ],
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: SizedBox(
-                  width: 70,
-                  height: 90,
-                  child: GestureDetector(
-                    onTap: () async {
-                      SharedPreferences prefs = await SharedPreferences.getInstance();
-                      if (prefs.get("sk1") != null && prefs.get("sk2")  != null) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    MainBuluntuList())); //***MainBuluntuList
-                      } else {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    LoginMain())); //***LoginMain
-                      }
-                    },
-                    child: Container(
-                        child: Image(
-                      image: AssetImage("img/right_arrow.png"),
-                    )),
+            ),
+            // --- Page 2 ---
+            Container(
+              margin: EdgeInsets.only(top: 60),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: MediaQuery.of(context).size.height / 3,
+                    margin: EdgeInsets.only(top: 80),
+                    child: Image(
+                      image: AssetImage("assets/icon-v1.png"),
+                    ),
                   ),
-                ),
+                  Container(
+                      margin: EdgeInsets.only(top: 40, left: 25, right: 25),
+                      child: Text(
+                        "Kolay, hızlı ve sıkıntısız.\n\nBuluntu kaydı\nKayıp eşya bildirme",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 25, fontFamily: "Ubuntu"),
+                      )),
+                  Container(
+                    height: 50,
+                    width: 80,
+                    margin:EdgeInsets.only(top: 80),
+                    child: ElevatedButton(
+                      onPressed: ()async {
+                        SharedPreferences prefs = await SharedPreferences.getInstance();
+                        if (prefs.get("sk1") != null && prefs.get("sk2")  != null) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      MainBuluntuList())); //***MainBuluntuList
+                        } else {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      LoginMain())); //***LoginMain
+                        }
+                      },
+                      child: Text("Atla",style: TextStyle(fontSize: 18,fontFamily: "Ubuntu"),),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-      ],
-    ));
+            ),
+          ],
+        ));
   }
 }
